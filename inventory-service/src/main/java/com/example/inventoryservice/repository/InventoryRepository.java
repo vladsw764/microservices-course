206389decbf382c87e11c7cd2722b3a38cd1550a
Vladyslav <vladsw764@icloud.com>
2023-07-06 19:54:41 +0300

    minor edits in InventoryRepository

--- inventory-service/src/main/java/com/example/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/example/inventoryservice/repository/InventoryRepository.java
@@ -3,8 +3,9 @@ package com.example.inventoryservice.repository;
 import com.example.inventoryservice.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
-    Optional<Inventory> findBySkuCode(String skuCode);
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
 }
