c40b3344e6b5325360775e77f3f0a897e0628c68
Vladyslav <vladsw764@icloud.com>
2023-09-19 10:32:29 +0300

    created UserInfoDto

---
+++ order-service/src/main/java/com/isariev/orderservice/dto/UserInfoDto.java
@@ -0,0 +1,16 @@
+package com.isariev.orderservice.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record UserInfoDto(
+        String sub,
+
+        @JsonProperty("email_verified")
+        boolean emailVerified,
+
+        @JsonProperty("preferred_username")
+        String preferredUsername,
+
+        String email
+) {
+}
