96fb6db8d6806a5ed63f22f6b308543b9b41098a
Vladyslav <vladsw764@icloud.com>
2023-09-19 10:32:57 +0300

    created WebClientConfig

---
+++ customer-service/src/main/java/com/isariev/customerservice/config/WebClientConfig.java
@@ -0,0 +1,21 @@
+package com.isariev.customerservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+
+    @Bean
+    public WebClient webClient(WebClient.Builder webClientBuilder) {
+        return webClientBuilder.baseUrl("http://localhost:8181")
+                .build();
+    }
+}
+
