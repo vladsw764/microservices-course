f745259f66003fe81396cef3b75bf18d101cb91c
Vladyslav <vladsw764@icloud.com>
2023-09-19 10:33:54 +0300

    added 'circuitbreaker', 'retry' & 'timelimiter'

--- order-service/src/main/java/com/isariev/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/isariev/orderservice/controller/OrderController.java
@@ -2,14 +2,20 @@ package com.isariev.orderservice.controller;
 
 import com.isariev.orderservice.dto.OrderRequest;
 import com.isariev.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
-import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,11 +24,19 @@ import java.util.List;
 public class OrderController {
 
     private final OrderService orderService;
+    private final HttpServletRequest request;
+
 
     @PostMapping
-    public void placeOrder(@RequestBody OrderRequest orderRequest, @RequestHeader("X-User-Id") String userId) {
+    @CircuitBreaker(name = "order-service")
+    @Retry(name = "order-service")
+    @TimeLimiter(name = "order-service")
+    public CompletionStage<Void> placeOrder(@RequestBody OrderRequest orderRequest, HttpServletRequest request) {
         log.info("Attempting to place an order.");
-        orderService.placeOrder(orderRequest, userId);
+
+        return CompletableFuture.runAsync(() -> {
+            orderService.placeOrder(orderRequest, request);
+        });
     }
 }
 
