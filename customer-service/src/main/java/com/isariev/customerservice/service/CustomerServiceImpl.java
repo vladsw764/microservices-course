5e376600ca83e2a187ca1c1448b2b9d90231134f
Vladyslav <vladsw764@icloud.com>
2023-09-19 10:30:00 +0300

    added method for getting user info

--- customer-service/src/main/java/com/isariev/customerservice/service/CustomerServiceImpl.java
+++ customer-service/src/main/java/com/isariev/customerservice/service/CustomerServiceImpl.java
@@ -6,8 +6,10 @@ import com.isariev.customerservice.dto.OrderInfoDto;
 import com.isariev.customerservice.dto.mapper.CustomerMapper;
 import com.isariev.customerservice.model.Customer;
 import com.isariev.customerservice.repository.CustomerRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,14 +20,33 @@ public class CustomerServiceImpl {
     private final static String TOPIC_NEW = "customer-order-topic";
 
     private final CustomerRepository customerRepository;
+
+    private final HttpServletRequest request;
+
+    private final WebClient webClient;
+
     private final CustomerMapper customerMapper;
 
-    public CustomerServiceImpl(CustomerRepository customerRepository, CustomerMapper customerMapper) {
+    public CustomerServiceImpl(CustomerRepository customerRepository, HttpServletRequest request, WebClient webClient, CustomerMapper customerMapper) {
         this.customerRepository = customerRepository;
+        this.request = request;
+        this.webClient = webClient;
         this.customerMapper = customerMapper;
     }
 
     public OrderInfoDto getCountOfOrdersById(String customerId) {
+        int count = customerRepository.findAllByCustomerId(customerId).size();
+
+        return new OrderInfoDto(
+                count,
+                customerId,
+                "Text"
+        );
+    }
+
+    public OrderInfoDto getCountOfOrders() {
+        String customerId = getCustomerId();
+
         int count = customerRepository.findAllByCustomerId(customerId).size();
 
         return new OrderInfoDto(
@@ -51,5 +72,16 @@ public class CustomerServiceImpl {
         customerRepository.save(customer);
         System.out.println(customer);
     }
+
+    private String getCustomerId() {
+        String bearer = request.getHeader("Authorization").substring("Bearer ".length());
+        return webClient.get()
+                .uri("/userinfo/id")
+                .headers(header -> header.setBearerAuth(bearer))
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
+
 }
 
