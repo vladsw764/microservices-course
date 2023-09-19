fb6f007a5236045b6b987a04ff1cb2a31bbe49bf
Vladyslav <vladsw764@icloud.com>
2023-09-19 10:30:23 +0300

    configured kafka topic config

--- order-service/src/main/java/com/isariev/orderservice/config/kafkaConfig/KafkaTopicConfig.java
+++ order-service/src/main/java/com/isariev/orderservice/config/kafkaConfig/KafkaTopicConfig.java
@@ -1,6 +1,7 @@
 package com.isariev.orderservice.config.kafkaConfig;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
@@ -8,9 +9,30 @@ import org.springframework.kafka.config.TopicBuilder;
 @Configuration
 public class KafkaTopicConfig {
 
+    @Value("${spring.kafka.topics.inventory}")
+    private String inventoryTopic;
+
+    @Value("${spring.kafka.topics.order}")
+    private String orderTopic;
+
+    @Value("${spring.kafka.topics.customer}")
+    private String customerTopic;
+
     @Bean
     public NewTopic orderInventoryTopic() {
-        return TopicBuilder.name("order-inventory-topic")
+        return TopicBuilder.name(orderTopic)
+                .build();
+    }
+
+    @Bean
+    public NewTopic inventoryOrderTopic() {
+        return TopicBuilder.name(inventoryTopic)
+                .build();
+    }
+
+    @Bean
+    public NewTopic customerOrderTopic() {
+        return TopicBuilder.name(customerTopic)
                 .build();
     }
 }
