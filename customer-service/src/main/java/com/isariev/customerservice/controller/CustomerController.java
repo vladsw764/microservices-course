6ef66b7af97c9322b0fe5757ccc341d55667fe8c
Vladyslav <vladsw764@icloud.com>
2023-09-19 10:29:01 +0300

    added 'getCountOfOrders' endpoint

--- customer-service/src/main/java/com/isariev/customerservice/controller/CustomerController.java
+++ customer-service/src/main/java/com/isariev/customerservice/controller/CustomerController.java
@@ -1,10 +1,13 @@
 package com.isariev.customerservice.controller;
 
 import com.isariev.customerservice.dto.OrderDiscountDto;
+import com.isariev.customerservice.dto.OrderInfoDto;
 import com.isariev.customerservice.service.CustomerServiceImpl;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.server.ServerRequest;
 
 @RestController
 @RequestMapping("/api/customer")
@@ -16,12 +19,17 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
-//    @GetMapping
-//    public OrderInfoDto getCountOfOrders(@RequestParam("customerId") String customerId) {
-//        return customerService.getCountOfOrdersById(customerId);
-//    }
+    @GetMapping
+    public OrderInfoDto getCountOfOrdersById(@RequestParam("customerId") String customerId) {
+        return customerService.getCountOfOrdersById(customerId);
+    }
 
-    @GetMapping()
+    @GetMapping("/count")
+    public OrderInfoDto getCountOfOrders() {
+        return customerService.getCountOfOrders();
+    }
+
+    @GetMapping("/discount")
     public OrderDiscountDto getOrdersInMonth() {
         return customerService.getOrdersInMonth();
     }
