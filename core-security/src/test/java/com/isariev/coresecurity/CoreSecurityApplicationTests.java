d47a666446a49ed586ab85cc45e663888c273609
Vladyslav <vladsw764@icloud.com>
2023-12-11 20:55:19 +0200

    add core security

---
+++ core-security/src/test/java/com/isariev/coresecurity/CoreSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isariev.coresecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- order-service/src/main/java/com/isariev/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/isariev/orderservice/OrderServiceApplication.java
@@ -3,8 +3,10 @@ package com.isariev.orderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@ComponentScan({"com.isariev.coresecurity", "com.isariev.orderservice"})
+@EnableJpaRepositories({"com.isariev.orderservice", "com.isariev.coresecurity"})
+@ComponentScan({"com.isariev.orderservice", "com.isariev.coresecurity"})
 @SpringBootApplication
 public class OrderServiceApplication {
 
